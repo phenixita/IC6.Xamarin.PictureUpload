b86e3a090b2ef3b152f444b9ba5dffe601a49a34
mibzman <sam@hungerperks.com>
2019-02-28 15:15:30 -0500

    mcs compatibility changes

--- IC6.Xamarin.PictureUpload/IC6.Xamarin.PictureUpload/ApiService.cs
+++ IC6.Xamarin.PictureUpload/IC6.Xamarin.PictureUpload/ApiService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,19 +7,39 @@ namespace IC6.Xamarin.PictureUpload
 {
     internal class ApiService : IApiService
     {
-        private string url = "http://localhost:3573/api/image/";
+
+        private string url = "http://10.9.2.2:3574/api/image/";
 
         public async Task<bool> UploadImageAsync(Stream image, string fileName)
         {
+
             HttpContent fileStreamContent = new StreamContent(image);
             fileStreamContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data") { Name = "file", FileName = fileName };
             fileStreamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
             using (var client = new HttpClient())
+
+                try
+                {
+                    await client.GetAsync("http://10.9.2.2:3574/api/values/");
+                } catch (Exception e)
+                {
+                    return false;
+                }
+            using (var client = new HttpClient())
             using (var formData = new MultipartFormDataContent())
             {
                 formData.Add(fileStreamContent);
-                var response = await client.PostAsync(url, formData);
-                return response.IsSuccessStatusCode;
+                try
+                {
+                    var response = await client.PostAsync(url, formData);
+                    return response.IsSuccessStatusCode;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+
+
             }
         }
     }
